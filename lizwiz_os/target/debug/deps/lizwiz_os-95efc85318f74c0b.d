0611358da7170abd3ee0015ff92f50af2a53149b
Stijn Spruit <spr@deviGroup.local>
2025-10-24 12:00:10 +0200

    initialised rust project

---
+++ lizwiz_os/target/debug/deps/lizwiz_os-95efc85318f74c0b.d
@@ -0,0 +1,5 @@
+C:\Users\spr\docs\lizwiz_os_from_scratch\lizwiz_os\target\debug\deps\lizwiz_os-95efc85318f74c0b.d: src\main.rs
+
+C:\Users\spr\docs\lizwiz_os_from_scratch\lizwiz_os\target\debug\deps\liblizwiz_os-95efc85318f74c0b.rmeta: src\main.rs
+
+src\main.rs:
---
+++ lizwiz_os/target/debug/deps/lizwiz_os-b72ebeed33b3f98c.d
@@ -0,0 +1,5 @@
+C:\Users\spr\docs\lizwiz_os_from_scratch\lizwiz_os\target\debug\deps\lizwiz_os-b72ebeed33b3f98c.d: src\main.rs
+
+C:\Users\spr\docs\lizwiz_os_from_scratch\lizwiz_os\target\debug\deps\liblizwiz_os-b72ebeed33b3f98c.rmeta: src\main.rs
+
+src\main.rs:
